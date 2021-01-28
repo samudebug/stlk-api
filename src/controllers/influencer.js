a818aefd525da532261d20ee6604088c9e23d1f6
Samuel Martins <samuel.ubuntu@gmail.com>
2021-01-28 16:03:46 -0300

    Get influencer route

--- src/controllers/influencer.js
+++ src/controllers/influencer.js
@@ -2,7 +2,7 @@ import * as admin from 'firebase-admin';
 class InfluencerController {
     async list(req, res) {
         try {
-            const userQuery = await admin.firestore().collection('users').where('uid', '==', req.id).limit(1).get();
+            const userQuery = await admin.firestore().collection('users').where('uid', '==', req.uid).limit(1).get();
             const [user] = userQuery.docs;
             const result = user.data().influencers === undefined ? [] : [...user.data().influencers];
             return res.send(result);
