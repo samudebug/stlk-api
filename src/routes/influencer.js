8ee668be31ef27976564eeab54ce1dca07f5d2ae
Samuel Martins <samuel.ubuntu@gmail.com>
2021-01-28 22:13:45 -0300

    Create influencer funtion in router

--- src/routes/influencer.js
+++ src/routes/influencer.js
@@ -6,5 +6,6 @@ const router = express.Router();
 const influencerController = new InfluencerController();
 
 router.get('/', (req, res) => influencerController.list(req, res));
+router.post('/', (req, res) => influencerController.create(req, res));
 
 export default router;
